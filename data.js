5573da02ea1fad735999aec0b350d34d074cc3a9
Kalan Prudhomme <cleaver9@gmail.com>
2021-04-21 10:04:03 -0700

    added more data and new assets

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-export const cubbies = {
+const cubbies = {
 
     id: 'cubbies',
     title: 'Cubbies',
@@ -154,7 +154,585 @@ export const cubbies = {
 
 
 };
+const cats = {
 
+    id: 'cats',
+    title: 'Cats',
+    image: '../assets/cats.jpg',
+
+    hiddenObjects: [
+        {
+            id: 'lion',
+            clue: 'a lion,',
+            description: 'a lion',
+            image: '../assets/lion.png',
+            map: {
+                top: '50%',
+                left: '50%'
+            }
+
+            // dimensions: {
+            //     width: '200',
+            //     height: '200',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'shell',
+            clue: 'a shell from the ocean,',
+            description: 'a sea shell',
+            image: '../assets/shell.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'fish',
+            clue: 'a fish who wears hats,',
+            description: 'a fish wearing a hat',
+            image: '../assets/pencil.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'horse',
+            clue: 'a horse that rocks,',
+            description: 'rocking horse',
+            image: '../assets/horse.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'puppy',
+            clue: 'a little puppy dog,',
+            description: 'a little puppy dog',
+            image: '../assets/puppy.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'button',
+            clue: 'a button with a square',
+            description: 'a button',
+            image: '../assets/button.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat1',
+            clue: 'a cat that\'s black',
+            description: 'a black cat',
+            image: '../assets/cat1.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat2',
+            clue: 'a cat that hisses',
+            description: 'a hissing cat',
+            image: '../assets/cat2.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat3',
+            clue: 'a dapper cat with a big red bow',
+            description: 'a handsome cat',
+            image: '../assets/cat3.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat4',
+            clue: 'a cat with a fiddle',
+            description: 'a musical cat',
+            image: '../assets/cat4.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat5',
+            clue: 'a crying cat',
+            description: 'a crying cat',
+            image: '../assets/cat5.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat6',
+            clue: 'a cat on a fence',
+            description: 'an old maids cat',
+            image: '../assets/cat6.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat7',
+            clue: 'a baby cat',
+            description: 'a kitty cat',
+            image: '../assets/cat7.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'cat8',
+            clue: 'a cat with a yellow bow',
+            description: 'a yellow cat',
+            image: '../assets/cat8.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+
+    ],
+
+
+};
+const whitespace = {
+
+    id: 'whitespace',
+    title: 'Whitespace',
+    image: '../assets/whitespace.jpg',
+
+    hiddenObjects: [
+        {
+            id: 'eagle',
+            clue: 'an eagle,',
+            description: 'an eagle',
+            image: '../assets/eagle.png',
+            map: {
+                top: '50%',
+                left: '50%'
+            }
+
+            // dimensions: {
+            //     width: '200',
+            //     height: '200',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'paperclip',
+            clue: 'a yellow paperclip,',
+            description: 'a yellow paperclip',
+            image: '../assets/paperclip.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'spider',
+            clue: 'a black spider,',
+            description: 'a spider',
+            image: '../assets/spider.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'shoe',
+            clue: 'a pink baby shoe,',
+            description: 'a pink shoe',
+            image: '../assets/shoe.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'phone',
+            clue: 'a bright red phone,',
+            description: 'a red phone',
+            image: '../assets/phone.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'fish',
+            clue: 'a red fish',
+            description: 'a red fish',
+            image: '../assets/fish.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'corn',
+            clue: 'an ear of corn',
+            description: 'a corn cob',
+            image: '../assets/corn.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'hat',
+            clue: 'a hat that\'s blue',
+            description: 'a blue hat',
+            image: '../assets/hat.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+
+    ],
+
+
+};
+const beads = {
+
+    id: 'beads',
+    title: 'Bead Explosion',
+    image: '../assets/bead-explosion.jpg',
+
+    hiddenObjects: [
+        {
+            id: 'banana',
+            clue: 'a beady banana,',
+            description: 'a banana',
+            image: '../assets/banana.png',
+            map: {
+                top: '50%',
+                left: '50%'
+            }
+
+            // dimensions: {
+            //     width: '200',
+            //     height: '200',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'teddy-bear',
+            clue: 'a smiling teddy bear,',
+            description: 'a yellow teddy bear',
+            image: '../assets/teddy-bear.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'squirrel',
+            clue: 'a little squirrel,',
+            description: 'a little squirrel',
+            image: '../assets/squirrel.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'giraffe',
+            clue: 'a lost giraffe,',
+            description: 'a silver giraffe',
+            image: '../assets/giraffe.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'dragonfly',
+            clue: 'a dragonfly',
+            description: 'a dragonfly',
+            image: '../assets/dragonfly.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'teddy-bear2',
+            clue: 'a tied up bear',
+            description: 'a teddybear with a red bow',
+            image: '../assets/teddy-bear2.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'telephone',
+            clue: 'a silver telephone',
+            description: 'a silver telephone',
+            image: '../assets/telephone.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+        {
+            id: 'fan',
+            clue: 'a bead fan',
+            description: 'a small blue fan',
+            image: '../assets/fan.png',
+            map: {
+                top: '',
+                left: ''
+            }
+
+            // dimensions: {
+            //     width: '',
+            //     height: '',
+            //     borderRadius: ''
+            // }
+
+
+        },
+
+    ],
+
+
+};
 export const puzzles = [
-    cubbies
+    cubbies,
+    cats,
+    whitespace,
+    beads
 ];

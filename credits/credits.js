7559909c77421bcb9c4e3e45797eef6aa5289f34
Daniella Marks <marks.daniella@gmail.com>
2021-04-21 19:10:08 -0700

    accidentally working here, fixed credits css

--- credits/credits.js
+++ credits/credits.js
@@ -1,3 +1,8 @@
 import { renderProfile } from '../utils.js';
 
 renderProfile();
+
+const kat = document.querySelector('.kat-photo');
+const daniella = document.querySelector('.d-photo');
+const kalan = document.querySelector('.kalan-photo');
+

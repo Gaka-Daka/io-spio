5718aedfca68a8b55e0bab3a174ef623e10fee30
Daniella Marks <marks.daniella@gmail.com>
2021-04-21 22:42:09 -0700

    blurbs and event listeners

--- credits/credits.js
+++ credits/credits.js
@@ -5,4 +5,41 @@ renderProfile();
 const kat = document.querySelector('.kat-photo');
 const daniella = document.querySelector('.d-photo');
 const kalan = document.querySelector('.kalan-photo');
+const gabriel = document.querySelector('.gabriel-photo');
 
+const katDescript = document.getElementById('kat-description');
+const kalanDescript = document.getElementById('kalan-description');
+const danDescript = document.getElementById('daniella-description');
+const gabDescript = document.getElementById('gabriel-description');
+
+const kalClue = document.getElementById('kal-clue')
+const katClue = document.getElementById('kat-clue')
+const danClue = document.getElementById('dan-clue')
+const gabClue = document.getElementById('gab-clue')
+
+kat.addEventListener('click', () => {
+    kat.classList.add('drop-shadow');
+    katDescript.classList.remove('hidden');
+    katClue.classList.add('line-through');
+});
+
+daniella.addEventListener('click', () => {
+    daniella.classList.add('drop-shadow');
+    danDescript.classList.remove('hidden');
+    danClue.classList.add('line-through');
+
+});
+
+kalan.addEventListener('click', () => {
+    kalan.classList.add('drop-shadow');
+    kalanDescript.classList.remove('hidden');
+    kalClue.classList.add('line-through');
+
+});
+
+gabriel.addEventListener('click', () => {
+    gabriel.classList.add('drop-shadow');
+    gabDescript.classList.remove('hidden');
+    gabClue.classList.add('line-through');
+
+});

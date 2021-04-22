f804afab6a0435e90bb3253bdde8790635a5fb1e
Kalan Prudhomme <cleaver9@gmail.com>
2021-04-22 16:32:08 -0700

    added audio player

--- game-config/config.js
+++ game-config/config.js
@@ -44,3 +44,4 @@ const audioButton = document.getElementById('audio');
 audioButton.addEventListener('click', () => {
     window.open('../music', 'popUpWindow', 'height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 });
+
